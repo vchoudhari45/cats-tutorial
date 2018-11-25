6ce4a57ffb926aeff6e88b9bd1f5862256d40402
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2018-11-25 01:33:43 -0800

    CustomMonad

--- src/main/scala/com/vc/6. StateMonad.scala
+++ src/main/scala/com/vc/6. StateMonad.scala
@@ -1,5 +1,32 @@
 package com.vc
 
+/**
+  * StateMonad allows us to pass additional state around as part of a computation
+  *
+  **/
 object StateMonad {
+
+  def main(args: Array[String]): Unit = {
+    import cats.data.State
+
+    val step1 = State[Int, String] { num =>
+      val ans = num + 1
+      (ans, s"Result of step1: $ans")
+    }
+
+    val step2 = State[Int, String] { num =>
+      val ans = num * 2
+      (ans, s"Result of step2: $ans")
+    }
+
+    val both = for {
+      a <- step1
+      b <- step2
+    } yield (a, b)
+
+    val (state, result) = both.run(20).value
+    println(s"State: $state Result: $result") //State: 42 Result: (Result of step1: 21,Result of step2: 42)
+
+  }
 
 }
--- src/main/scala/com/vc/7. CustomMonad.scala
+++ src/main/scala/com/vc/7. CustomMonad.scala
@@ -1,5 +1,28 @@
 package com.vc
 
-class CustomMonad {
+/**
+  * We can define a Monad for a custom type by providing implementation of three methods
+  * flatmap, pure & tailRecM
+  **/
+object CustomMonad {
+
+  def main(args: Array[String]): Unit = {
+    import cats.Monad
+    import scala.annotation.tailrec
+
+    val optionMonad = new Monad[Option] {
+      def flatMap[A, B](opt: Option[A])(fn: A => Option[B]): Option[B] = opt flatMap fn
+
+      def pure[A](opt: A): Option[A] = Some(opt)
+
+      @tailrec
+      def tailRecM[A, B](a: A)(fn: A => Option[Either[A, B]]): Option[B] =
+        fn(a) match {
+          case None           => None
+          case Some(Left(a1)) => tailRecM(a1)(fn)
+          case Some(Right(b)) => Some(b)
+        }
+    }
+  }
 
 }
